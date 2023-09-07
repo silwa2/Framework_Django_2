40911c74ca511e0f11eec1e5d15a44a4150a38dc
Aleksander Sinkov <silwa2@yandex.ru>
2023-09-07 08:09:41 +0300

    Django_Seminar_3

--- task_django/urls.py
+++ task_django/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from magazin_app.views import TemplIf
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('my_app.urls')),
+    path('magazin/', include('magazin_app.urls')),
+    path('orders/<int:id_user>', TemplIf.as_view(), name='orders'),
 ]
