3239c2890978877d2ee001f17cfc1bc6fa2a60d0
Aleksander Sinkov <silwa2@yandex.ru>
2023-09-14 15:36:24 +0300

    Django_Seminar_6

--- task_django/settings.py
+++ task_django/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,16 +22,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=fp8@xa2@r%dls7e1!2*!hkd7bw_bye2z8y#42-c5t%bp5095g'
+# SECRET_KEY = 'django-insecure-=fp8@xa2@r%dls7e1!2*!hkd7bw_bye2z8y#42-c5t%bp5095g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
     '192.168.0.11',
+    'silwa2.pythonanywhere.com',
 ]
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,11 +51,14 @@ INSTALLED_APPS = [
     'my_app2',
     'magazin_app',
     'myapp4',
-    'myapp5'
+    'myapp5',
+    'myapp6',
+    'debug_toolbar',
 
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,7 +73,8 @@ ROOT_URLCONF = 'task_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates',
+        'DIRS': [
+            BASE_DIR / 'templates',
                  ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -82,8 +95,15 @@ WSGI_APPLICATION = 'task_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'silwa2$default',
+        'USER': 'silwa2',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'silwa2.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -120,6 +140,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
